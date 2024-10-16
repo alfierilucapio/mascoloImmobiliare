18cb651aa28461fcb44eb3b79c4e0752bf074781
alfierilucapio <alfierilucapio@gmail.com>
2024-10-16 13:40:32 +0200

    correzioni

--- assets/js/Caricamento.js
+++ assets/js/Caricamento.js
@@ -4,25 +4,25 @@ let pageLoaded = false;
 let DOMContentLoaded = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-    DOMContentLoaded=true;
+    DOMContentLoaded = true;
 });
 
-window.onload = function(){
+window.onload = function() {
     pageLoaded = true;
 }
 
 // Funzione da eseguire al termine della GIF
 function onGifEnd() {
-    if(pageLoaded && DOMContentLoaded){
+    if (pageLoaded && DOMContentLoaded) {
         const div = document.getElementById('schermata_caricamento');
         div.classList.add('hidden');
         setTimeout(() => {
             div.style.display = 'none';
         }, 500); // Tempo di transizione in millisecondi
-    }else{
-        gifDuration = gifDuration + 1810;
+    } else {
+        setTimeout(onGifEnd, gifDuration); // Esegui di nuovo la funzione dopo la durata della GIF
     }
 }
 
 // Esegui la funzione al termine della durata della GIF
-setTimeout(onGifEnd, gifDuration)
+setTimeout(onGifEnd, gifDuration);
