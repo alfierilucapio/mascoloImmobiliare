95a2a0aad486b722de5801d30e6eec861f480a1d
alfierilucapio <alfierilucapio@gmail.com>
2024-10-16 13:43:39 +0200

    aggiunta di console log

--- assets/js/Caricamento.js
+++ assets/js/Caricamento.js
@@ -4,15 +4,18 @@ let pageLoaded = false;
 let DOMContentLoaded = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded");
     DOMContentLoaded = true;
 });
 
 window.onload = function() {
+    console.log("window.onload");
     pageLoaded = true;
 }
 
 // Funzione da eseguire al termine della GIF
 function onGifEnd() {
+    console.log("onGifEnd");
     if (pageLoaded && DOMContentLoaded) {
         const div = document.getElementById('schermata_caricamento');
         div.classList.add('hidden');
@@ -20,6 +23,7 @@ function onGifEnd() {
             div.style.display = 'none';
         }, 500); // Tempo di transizione in millisecondi
     } else {
+        console.log("Ritardo aggiuntivo");
         setTimeout(onGifEnd, gifDuration); // Esegui di nuovo la funzione dopo la durata della GIF
     }
 }
