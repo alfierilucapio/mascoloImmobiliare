1f4217d828367c1aab12fb2dd0818251a99c3566
alfierilucapio <alfierilucapio@gmail.com>
2024-10-19 12:40:41 +0200

    modifica caricamento

--- assets/js/Caricamento.js
+++ assets/js/Caricamento.js
@@ -1,32 +1,24 @@
-// Durata della GIF in millisecondi
-const gifDuration = 1810;
+// Durata della GIF in millisecondi (esempio: 5 secondi)
+const gifDuration = 1710;
 let pageLoaded = false;
-let DOMContentLoaded = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOMContentLoaded");
-    DOMContentLoaded = true;
+    pageLoaded=true;
+    document.getElementById('video_caricamento').style.display = 'block';
 });
-
-window.onload = function() {
-    console.log("window.onload");
-    pageLoaded = true;
-}
 
 // Funzione da eseguire al termine della GIF
 function onGifEnd() {
-    console.log("onGifEnd");
-    if (pageLoaded && DOMContentLoaded) {
+    if(pageLoaded){
         const div = document.getElementById('schermata_caricamento');
         div.classList.add('hidden');
         setTimeout(() => {
             div.style.display = 'none';
         }, 500); // Tempo di transizione in millisecondi
-    } else {
-        console.log("Ritardo aggiuntivo");
-        setTimeout(onGifEnd, gifDuration); // Esegui di nuovo la funzione dopo la durata della GIF
+    }else{
+        gifDuration = gifDuration + 1710;
     }
 }
 
 // Esegui la funzione al termine della durata della GIF
-setTimeout(onGifEnd, gifDuration);
+setTimeout(onGifEnd, gifDuration)
