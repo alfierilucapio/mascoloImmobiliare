5c05dee35b424bea58911ba4d423a45b030b6a7c
alfierilucapio <alfierilucapio@gmail.com>
2024-11-11 17:35:24 +0100

    correzione caricamento finale

--- assets/js/Caricamento.js
+++ assets/js/Caricamento.js
@@ -37,8 +37,12 @@ function onGifEnd() {
     }
 }
 
+// Evento DOMContentLoaded per indicare che la pagina è caricata
+document.addEventListener("DOMContentLoaded", () => {
+    mostraGifCaricamento();
+});
+
 // Evento load per indicare che tutte le risorse della pagina sono caricate
 window.addEventListener("load", () => {
     pageLoaded = true;
-    mostraGifCaricamento();
 });
